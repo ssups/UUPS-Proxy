46df8813754cabb0f4f4ea57bb8b95bdbe599c2a
ssups <shalstlqruki@gmail.com>
2023-08-31 18:57:18 +0900

    fix: expect value type & block for upgradeTo

--- test/Deploy&Upgrdade.test.ts
+++ test/Deploy&Upgrdade.test.ts
@@ -1,12 +1,13 @@
 import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
 import { Contract } from "ethers";
+import { makeUpgradeProxy } from "@openzeppelin/hardhat-upgrades/dist/upgrade-proxy";
 
 describe("deploy & upgrade", () => {
   let proxyCA: string;
   let proxyC: Contract;
 
-  before("first deploy", async () => {
+  it("first deploy", async () => {
     const INIT_MINT_AMOUNT = 100_000_000;
     const factoryV1 = await ethers.getContractFactory("TestTokenV1");
     proxyC = await upgrades.deployProxy(factoryV1, [INIT_MINT_AMOUNT], {
@@ -16,28 +17,28 @@ describe("deploy & upgrade", () => {
     await proxyC.waitForDeployment();
     proxyCA = await proxyC.getAddress();
 
-    expect(await proxyC.version()).to.eq("1");
+    expect((await proxyC.version()).toString()).to.eq("1");
   });
 
   it("upgrade to V2", async () => {
     const factoryV2 = await ethers.getContractFactory("TestTokenV2");
-    await upgrades.upgradeProxy(proxyCA, factoryV2, { kind: "uups" });
-
-    expect(await proxyC.version()).to.eq("2");
+    const upgradedProxy = await upgrades.upgradeProxy(proxyCA, factoryV2, { kind: "uups" });
+    await upgradedProxy.deployTransaction.wait();
+    expect((await proxyC.version()).toString()).to.eq("2");
   });
 
   it("disable upgrade", async () => {
     const tx = await proxyC.disableUpgrade();
     await tx.wait();
 
-    expect(await proxyC.isUpgradable()).to.eq("2");
+    expect((await proxyC.isUpgradable()).toString()).to.eq("2");
   });
 
   it("try to upgrade V3", async () => {
-    const factoryV4 = await ethers.getContractFactory("TestTokenV3");
+    const factoryV3 = await ethers.getContractFactory("TestTokenV3");
 
     // deploy implement contract
-    const implementCA = await upgrades.prepareUpgrade(proxyCA, factoryV4, {
+    const implementCA = await upgrades.prepareUpgrade(proxyCA, factoryV3, {
       kind: "uups",
     });
 
